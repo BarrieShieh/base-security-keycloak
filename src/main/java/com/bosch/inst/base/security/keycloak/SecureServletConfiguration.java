d190d06163f8c9814389105a3333b8d1af7e81eb
XAB1SGH <Xky19870905>
2021-03-03 15:35:49 +0800

    Add Auditor provider

--- src/main/java/com/bosch/inst/base/security/keycloak/SecureServletConfiguration.java
+++ src/main/java/com/bosch/inst/base/security/keycloak/SecureServletConfiguration.java
@@ -2,12 +2,14 @@ package com.bosch.inst.base.security.keycloak;
 
 import com.bosch.inst.base.security.keycloak.login.LoginController;
 import com.bosch.inst.base.security.keycloak.service.impl.KeycloakService;
+import com.bosch.inst.base.security.keycloak.servlet.AuditorProvider;
 import com.bosch.inst.base.security.keycloak.servlet.SecurityConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({
     SecurityConfiguration.class,
+    AuditorProvider.class,
     KeycloakService.class,
     LoginController.class
 })
